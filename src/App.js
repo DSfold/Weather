8141c40a0de3f7a8f801b8aaf444c27752f3df22
DSF <verdictsguilty@gmail.com>
2021-08-06 04:46:36 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { useDispatch, useSelector} from 'react-redux'
+import Search from './components/Search/Search';
+import Weather from './components/Weather/Weather';
+import Alert from './components/Alert/Alert';
+import { setAlert } from './store/actions/alertActions';
+import { setError } from './store/actions/weatherActions';
 
 function App() {
+  const dispatch = useDispatch();
+  const weatherData = useSelector((state)=>state.weather.data);
+  const loading = useSelector((state)=>state.weather.loading);
+  const error = useSelector((state)=>state.weather.error);
+  const alertMsg = useSelector((state)=>state.weather.message);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div>
+          <Search title='Enter city name to get the weather'/>
+          {loading ? <h2>Loading...</h2> : weatherData && <Weather data={weatherData}/>}
+          {alertMsg && <Alert message={alertMsg} onClose={()=>dispatch(setAlert(''))}/>}
+          {error && <Alert message={error} onClose={()=>dispatch(setError())}/>}
+        </div>
     </div>
   );
 }
